7607ab409b817602e1215138e653653cf7a5c16f
MitsukiUsui <mitsuo.com.mu@gmail.com>
2018-08-26 11:02:07 +0900

    NOBUG: adjusted to new mmseqs createdb command

--- bin/mmseqs/createdb.sh
+++ bin/mmseqs/createdb.sh
@@ -4,5 +4,8 @@ set -ue
 fastaFilepath=${1}
 dbFilepath=${2}
 
+tmpDirec=/var/tmp/${USER}/mmseqs/`date +%s`
+mkdir -p ${tmpDirec}
+
 mmseqs createdb ${fastaFilepath} ${dbFilepath}
-mmseqs createindex ${dbFilepath}
+mmseqs createindex ${dbFilepath} ${tmpDirec} --threads 30
